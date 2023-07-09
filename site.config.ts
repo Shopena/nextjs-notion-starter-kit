00a39b8a1fa57db8f9737aa8ba10e1a52f968c71
Shopena <139030243+Shopena@users.noreply.github.com>
2023-07-09 18:10:13 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: '@shopena__taiwan',
   github: 'Shopena',
-  linkedin: '証宇-蔡-755471200',
+  linkedin: 'www.linkedin.com/in/証宇-蔡-755471200',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
